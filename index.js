3c6a894d6ffe10c6ee0216e81ec290e30eb6a0de
DESKTOP-JKAO42L\Supun <kmsr777@gmail.com>
2018-09-06 16:37:12 +0530

    msg

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ app.post('/webhook', (req, res) => {
       res.sendStatus(200)
     }
   }else{
+    let event = messaging_events[0]
+    let sender = event.sender.id
     sendText(sender, "Text echo: undifined")
   }
 });
