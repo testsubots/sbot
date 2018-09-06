82a78f07714d6dff049b914b7d10580cdf0311b1
DESKTOP-JKAO42L\Supun <kmsr777@gmail.com>
2018-09-06 16:16:52 +0530

    msg

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const
-express = require('express'),
-bodyParser = require('body-parser'),
-app = express().use(bodyParser.json());
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
 
-app.listen(process.env.PORT || 8080, ()=>{
-	console.log('sbot webhook is listening')
+app.listen(process.env.PORT || 8080, () => {
+  console.log('sbot webhook is listening')
 });
 
 // // Creates the endpoint for our webhook 
 // app.post('/webhook', (req, res) => {  
- 
+
 //   let body = req.body;
 
 //   // Checks this is an event from a page subscription
@@ -35,20 +35,22 @@ app.listen(process.env.PORT || 8080, ()=>{
 
 // });
 
-app.post('/webhook', (req, res) => {  
-let messaging_events =  req.body.entry[0].messaging_events
-for(let i=0; i<messaging_events.length;i++){
-  let event = messaging_events[i]
-  let sender = event.sender.id
-  if(event.message && event.message.text){
-    let text = event.message.text
-    sendText(sender,"Text echo: "+ text.substring(0,100))
+app.post('/webhook', (req, res) => {
+  let messaging_events = req.body.entry[0].messaging_events
+  if (messaging_events != undefined) {
+    for (let i = 0; i < messaging_events.length; i++) {
+      let event = messaging_events[i]
+      let sender = event.sender.id
+      if (event.message && event.message.text) {
+        let text = event.message.text
+        sendText(sender, "Text echo: " + text.substring(0, 100))
+      }
+      res.sendStatus(200)
+    }
   }
-  res.sendStatus(200)
-}
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   return res.send('Welcome to Intuit Webhooks Sample App');
 });
 
@@ -59,44 +61,44 @@ app.get('/webhook', (req, res) => {
   let VERIFY_TOKEN = "50b0a8a5d38b4364d0933dfe8edff701"
   // let VERIFY_TOKEN = "EAAszq7UbuyYBAGaSYn8SK092ZCGSoRvhKtQvsFtTDOcEABwFv5T1VsatCbwWBo9WBTHfOMNwag5Wlrbd6qSq9PZABmb4AYR6E5jx7Bt9VHcSdrU7g8dzVOkUFjmqARYxQ9EjZCXdcueIZAeE3qFeDjQiVC6mVEz8m9OGsvQiexFSp1LTvoV6DjLZCRiDQwdAZD"
 
-    
+
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
+
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 
-function sendText(send,text){
-  let messageData = {text:text}
-  requestAnimationFrame({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs:{access_token, token},
-    method: "POST",
-    json:{
-      receipt: {id: sender},
-      message: messageData
-    },function(error,response, body){
-      if(error){
-        console.log('Sending an error')
-      }else if(response.body.error){
-        console.log("body error")
+  function sendText(send, text) {
+    let messageData = { text: text }
+    requestAnimationFrame({
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token, token },
+      method: "POST",
+      json: {
+        receipt: { id: sender },
+        message: messageData
+      }, function(error, response, body) {
+        if (error) {
+          console.log('Sending an error')
+        } else if (response.body.error) {
+          console.log("body error")
+        }
       }
-    }
-  })
-}
+    })
+  }
 });
