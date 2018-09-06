3f3f495f2562e44e21bf8c335d80bdc367c55739
DESKTOP-JKAO42L\Supun <kmsr777@gmail.com>
2018-09-06 17:22:52 +0530

    msg

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/webhook', (req, res) => {
 });
 function sendText(sender, text) {
   let messageData = { text: text }
-  request({
+  Request({
     url: "https://graph.facebook.com/v2.6/me/messages",
     qs: { access_token, token },
     method: "POST",
