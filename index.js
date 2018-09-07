ad089c30e7f55eb65f054af2d048403cee1ee15a
DESKTOP-JKAO42L\Supun <kmsr777@gmail.com>
2018-09-07 10:22:50 +0530

    msg

--- index.js
+++ index.js
@@ -40,9 +40,7 @@ let token = ""
 app.post('/webhook', (req, res) => {
   token = "subotnew";
   console.log(token)
-  console.log(JSON.stringify(req.body.entry[0].messaging[3].message.text))
-  console.log(JSON.stringify(req.body.entry[0].messaging.length))
-
+  console.log(JSON.stringify(req.body.entry[0].messaging[0].message.text))
   let messaging_events = req.body.entry[0].messaging
   if (messaging_events !== undefined) {
     for (let i = 0; i < messaging_events.length; i++) {
